2634b888d21a8bd21e6d5dc336254ce275b3ae3e
Daniel Gabriele <dg0offset@gmail.com>
2023-06-18 19:52:30 -0400

    removed deprecated function

--- src/util.rs
+++ src/util.rs
@@ -4,6 +4,8 @@ use cosmwasm_std::Uint128;
 
 use crate::models::{Claim, Drawing, Payout};
 
+const ONE_MILLION: u128 = 1_000_000;
+
 pub fn hash_numbers(numbers: &Vec<u16>) -> String {
   let parts: Vec<String> = numbers.iter().map(|n| n.to_string()).collect();
   parts.join(":")
@@ -16,20 +18,12 @@ pub fn mul_pct(
   total.multiply_ratio(pct, Uint128::from(1_000_000u128))
 }
 
-pub fn parse_hash(hash: &String) -> Vec<u16> {
-  hash
-    .split(":")
-    .map(|s| u16::from_str_radix(&s, 10).unwrap())
-    .collect()
-}
-
 pub fn calc_total_claim_amount(
   claim: &Claim,
   drawing: &Drawing,
   payouts: &HashMap<u8, Payout>,
 ) -> Uint128 {
   let mut claim_amount = Uint128::zero();
-  let one_mil = 1_000_000u32;
   for (n_matching_numbers, n_claim_tickets) in claim.match_counts.iter().enumerate().skip(1) {
     if let Some(payout) = payouts.get(&(n_matching_numbers as u8)) {
       let n_total_tickets = drawing.match_counts[n_matching_numbers] as u32;
@@ -39,8 +33,8 @@ pub fn calc_total_claim_amount(
 
       // Add portion of pot owed to user
       claim_amount += mul_pct(drawing.total_balance, payout.pct).multiply_ratio(
-        (*n_claim_tickets) as u32 * one_mil,
-        n_total_tickets * one_mil,
+        (*n_claim_tickets) as u128 * ONE_MILLION,
+        (n_total_tickets as u128) * ONE_MILLION,
       );
     }
   }
